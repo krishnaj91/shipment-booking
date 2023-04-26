03f2c29c9bf1d1bd2e13233944855e5c6ba9d32e
goutham <kgautham@appmindsglobal.com>
2023-04-26 15:51:52 +0530

    feature/shipment-booking

--- src/screens/boking/booking.tsx
+++ src/screens/boking/booking.tsx
@@ -1,6 +1,11 @@
-import { Button } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./booking.css";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import DoneIcon from "@mui/icons-material/Done";
+import CloseIcon from "@mui/icons-material/Close";
 
 const Booking = () => {
   const [form, setForm] = useState({
@@ -11,6 +16,8 @@ const Booking = () => {
     to: "",
     weight: "",
     amount: "",
+    date: "",
+    time: "",
   });
   const [formError, setFormError] = useState({
     id: "",
@@ -21,6 +28,11 @@ const Booking = () => {
     weight: "",
     amount: "",
   });
+  const [open, setOpen] = React.useState(false);
+  const [copy, setCopy] = React.useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const handleChange = (name: any, value: any) => {
     setForm((preValue) => ({ ...preValue, [name]: value }));
@@ -28,6 +40,28 @@ const Booking = () => {
   };
 
   const exceptThisSymbols = ["e", "E", "+", "-", "."];
+  // const currentDate = new Date().toLocaleString();
+
+  const today = new Date(); // current date
+  const daysToAdd = 2; // number of days to add
+  const options: any = { day: "2-digit", month: "2-digit", year: "numeric" };
+  const date = today.toLocaleDateString("en-IN", options); // outputs the current date in the Indian format DD/MM/YYYY,
+  const time = today.toLocaleTimeString(); // outputs the current time in the default format, e.g. 10:30:45 AM
+
+  const endDate = new Date(today);
+  endDate.setDate(today.getDate() + daysToAdd);
+  const formattedEndDate = endDate.toLocaleDateString("en-IN", options); // outputs the date after adding 2 days in DD/MM/YYYY format
+
+  function generateRandomString(length: any) {
+    var result = "";
+    var characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var charactersLength = 12;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
 
   const validate = () => {
     const tempObj = { ...formError };
@@ -58,78 +92,171 @@ const Booking = () => {
     setFormError(tempObj);
   };
 
-  function generateRandomString(length: any) {
-    var result = "";
-    var characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-  }
-
   const handleSubmit = () => {
     validate();
-    setForm({ ...form, id: generateRandomString(12) });
+    if (
+      form.name.length > 2 &&
+      form.mobile.length >= 10 &&
+      form.from.length > 2 &&
+      form.to.length > 2 &&
+      form.weight &&
+      form.amount
+    ) {
+      setForm({
+        ...form,
+        id: generateRandomString(12),
+        date: date,
+        time: time,
+      });
+      handleOpen();
+    }
   };
 
+  const handleCopy = () => {
+    navigator.clipboard.writeText(form.id);
+    setCopy(true);
+  };
+
+  useEffect(() => {
+    sessionStorage.setItem("order", form.id);
+  });
+
   return (
-    <div className="booking">
-      <h1>Fill the Details for booking your order</h1>
-      <input
-        className="inpt"
-        placeholder="name"
-        value={form.name}
-        onChange={(e) => handleChange("name", e.target.value)}
-      />
-      <p className="err">{formError.name}</p>
-      <input
-        className="inpt"
-        placeholder="mobile"
-        value={form.mobile}
-        type="number"
-        onWheel={(e: any) => e.target.blur()}
-        onKeyDown={(e) =>
-          exceptThisSymbols.includes(e.key) && e.preventDefault()
-        }
-        onChange={(e) =>
-          handleChange("mobile", e.target.value.toString().slice(0, 10))
-        }
-      />
-      <p className="err">{formError.mobile}</p>
-      <input
-        className="inpt"
-        placeholder="origin"
-        value={form.from}
-        onChange={(e) => handleChange("from", e.target.value)}
-      />
-      <p className="err">{formError.from}</p>
-      <input
-        className="inpt"
-        placeholder="destination"
-        value={form.to}
-        onChange={(e) => handleChange("to", e.target.value)}
-      />
-      <p className="err">{formError.to}</p>
-      <input
-        className="inpt"
-        placeholder="weight"
-        value={form.weight}
-        onChange={(e) => handleChange("weight", e.target.value)}
-      />
-      <p className="err">{formError.weight}</p>
-      <input
-        className="inpt"
-        type="number"
-        placeholder="amount"
-        value={form.amount}
-        onChange={(e) => handleChange("amount", e.target.value.toString().slice(0,4))}
-      />
-      <p className="err">{formError.amount}</p>
-      <button onClick={handleSubmit}>SUBMIT</button>
-    </div>
+    <>
+      <div className="booking">
+        <h1>Fill the Details for booking your order</h1>
+        <input
+          className="inpt"
+          placeholder="Name"
+          value={form.name}
+          onChange={(e) =>
+            handleChange("name", e.target.value.toString().slice(0, 15))
+          }
+        />
+        <p className="err">{formError.name}</p>
+        <input
+          className="inpt"
+          placeholder="Mobile Number"
+          value={form.mobile}
+          type="number"
+          onWheel={(e: any) => e.target.blur()}
+          onKeyDown={(e) =>
+            exceptThisSymbols.includes(e.key) && e.preventDefault()
+          }
+          onChange={(e) =>
+            handleChange("mobile", e.target.value.toString().slice(0, 10))
+          }
+        />
+        <p className="err">{formError.mobile}</p>
+        <input
+          className="inpt"
+          placeholder="Origin"
+          value={form.from}
+          onChange={(e) =>
+            handleChange("from", e.target.value.toString().slice(0, 15))
+          }
+        />
+        <p className="err">{formError.from}</p>
+        <input
+          className="inpt"
+          placeholder="Destination"
+          value={form.to}
+          onChange={(e) =>
+            handleChange("to", e.target.value.toString().slice(0, 15))
+          }
+        />
+        <p className="err">{formError.to}</p>
+        <input
+          className="inpt"
+          placeholder="Weight"
+          type="number"
+          onKeyDown={(e) =>
+            exceptThisSymbols.includes(e.key) && e.preventDefault()
+          }
+          value={form.weight}
+          onChange={(e) =>
+            handleChange("weight", e.target.value.toString().slice(0, 3))
+          }
+        />
+        <p className="err">{formError.weight}</p>
+        <input
+          className="inpt"
+          type="number"
+          onKeyDown={(e) =>
+            exceptThisSymbols.includes(e.key) && e.preventDefault()
+          }
+          placeholder="Amount"
+          value={form.amount}
+          onChange={(e) =>
+            handleChange("amount", e.target.value.toString().slice(0, 4))
+          }
+        />
+        <p className="err">{formError.amount}</p>
+        <button onClick={handleSubmit}>SUBMIT</button>
+      </div>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography className="cross">
+            <h3>
+              Shipment Booking <span className="green">SUCCESSFUL</span>
+            </h3>
+            <CloseIcon onClick={handleClose} />
+          </Typography>
+
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Your Order ID : <b>{form.id}</b>{" "}
+            {copy === false ? (
+              <span onClick={() => handleCopy()} className="copy">
+                copy
+              </span>
+            ) : (
+              <span className="copy">
+                copied
+                <DoneIcon />
+              </span>
+            )}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Order Date : <b className="red">{form.date}</b>
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Order Time : <b>{form.time}</b>
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Origin : <b>{form.from}</b>
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Destination : <b>{form.to}</b>
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Estimated Delivery : <b className="red">{formattedEndDate}</b>
+          </Typography>
+          <div className="modal-btn">
+          <Button variant="contained" onClick={handleClose}>
+            OK
+          </Button></div>
+        </Box>
+      </Modal>
+    </>
   );
 };
 
 export default Booking;
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
--- src/screens/tracking/tracking.tsx
+++ src/screens/tracking/tracking.tsx
@@ -4,6 +4,7 @@ import SearchIcon from '@mui/icons-material/Search';
 
 const Tracking = () => {
   const [search,setSearch]=useState('')
+  const [searchError,setSearchError]=useState<any>('')
 
   const handleKeyPress = (e:any) => {
     if (e.key === 'Enter') {
@@ -11,16 +12,40 @@ const Tracking = () => {
     }
   };
 
+  const handleChange = (e:any)=>{
+    setSearch(e)
+    setSearchError('')
+  }
+
+  const validate=()=>{
+    if(search===''){
+      setSearchError('Please Enter Your Order ID')
+    }
+    else if(search.length<12){
+      setSearchError('Invalid Order ID')
+    }
+    else if(search!==sessionStorage.getItem('order')){
+      setSearchError('Please Check Your Order ID (Shipment Not Found)')
+    }
+    else{
+      setSearchError('')
+    }
+  }
+
   const handleSubmit = ()=>{
-    alert('kj')
+    validate()
+    if(search===sessionStorage.getItem('order')){
+      alert('k')
+    }
   }
   return (
     <div className='tracking'>
       <h1>Enter your 12 Digit Order ID to check the Status</h1>
       <div className='search' >
         <SearchIcon className='search-icon' fontSize="large" onClick={handleSubmit}/>
-        <input placeholder='Search.......' type='search' value={search} className='search-inpt' onChange={(e:any)=>setSearch(e.target.value.toString().slice(0,12))} onKeyPress={handleKeyPress}/>
+        <input placeholder='Search.......' type='search' value={search} className='search-inpt' onChange={(e:any)=>handleChange(e.target.value.toString().slice(0,12).toUpperCase())} onKeyPress={handleKeyPress}/>
       </div>
+      <p className='red'>{searchError}</p>
     </div>
   )
 }
